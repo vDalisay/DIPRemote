d0b44a76b90daf8b9a46bdfdc805d40e129f2ffa
I384733 <I384733@fhict.nl>
2020-05-06 09:13:02 +0200

    NavBar works, maybe some new icons to be added. background still needs to be added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+import { MdAccountCircle } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
+import {Link} from "react-router-dom";
+import {MdAddBox} from "react-icons/md";
+
+const Navbar = props => (
+    <header className="navbar">
+        <nav className="navbar_navigation">
+            <div className="navbar_logo"><a href="/">Philips Workshops</a></div>
+            <div className="spacer"> </div>
+            <div className="navbar_navigation-items">
+                <ul>
+                    <li><Link to="/AllProjects"><MdSearch/></Link></li>
+                    <li><Link to="/CreateProject"><MdAddBox/></Link></li>
+                    <li><Link to="/Profile"><MdAccountCircle/></Link></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Navbar;
